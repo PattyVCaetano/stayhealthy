4b063edd3ba1db68ef3cb8e28cb6cf0d8b24b0c0
pattyvcaetano <patriciacaetano@hotmail.co.uk>
2024-04-24 10:51:59 -0400

    login js added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page'; // Import the Landing_Page component
+import Login from './Components/Login/Login'; // Import the Login component
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing_Page />} /> {/* Define the route for Landing_Page as the home route */}
-          {/* Add other routes here if needed */}
+          <Route path="/login" element={<Login />} /> {/* Define the route for Login */}
+        </Routes>
         </Routes>
       </div>
     </Router>

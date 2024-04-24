21a6e38ea1702e808bad395caaa0edab4dd61106
pattyvcaetano <patriciacaetano@hotmail.co.uk>
2024-04-24 10:58:04 -0400

    signup js added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page'; // Import the Landing_Page component
 import Login from './Components/Login/Login'; // Import the Login component
+import SignUp from './Components/Sign_Up/SignUp'; // Import the SignUp component
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing_Page />} /> {/* Define the route for Landing_Page as the home route */}
           <Route path="/login" element={<Login />} /> {/* Define the route for Login */}
+          <Route path="/signup" element={<SignUp />} /> {/* Define the route for SignUp */}
         </Routes>
         </Routes>
       </div>

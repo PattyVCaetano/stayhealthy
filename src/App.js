b9056a437541148d6f8915123b059e8df4273076
pattyvcaetano <pariciacaetano@hotmail.co.uk>
2024-04-26 06:28:01 -0400

    app.js update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page'; // Import the Landing_Page component
 import Login from './Components/Login/Login'; // Import the Login component
-import SignUp from './Components/Sign_Up/SignUp'; // Import the SignUp component
 
 function App() {
   return (
@@ -13,8 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing_Page />} /> {/* Define the route for Landing_Page as the home route */}
           <Route path="/login" element={<Login />} /> {/* Define the route for Login */}
-          <Route path="/signup" element={<SignUp />} /> {/* Define the route for SignUp */}
-        </Routes>
         </Routes>
       </div>
     </Router>

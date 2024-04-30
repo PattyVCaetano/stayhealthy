663d08e33b902b72e2bed30709ff9965f9f1f779
pattyvcaetano <patriciacaetano@hotmail.co.uk>
2024-04-30 07:52:03 -0400

    integrate landing page of website with Navbar

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,36 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./LandingPage.css";
+import Navbar from "../Navbar/Navbar"; // Import the Navbar component
 
 const Landing_Page = () => {
   return (
-    <section className="hero-section">
-      <div>
-        <div data-aos="fade-up" className="flex-hero">
-            <h1>
-              Your Health<br/>
-
-              <span className="text-gradient">
-                
-                Our Responsibility
-              </span>
-            </h1>
-              <div class="blob-cont">
-                  <div class="blue blob"></div>
-              </div>
-              <div class="blob-cont">
-                  <div class="blue1 blob"></div>
-              </div>
-            <h4>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
-            </h4>
-            <a href="#services">
-              <button class="button">Get Started</button>
-            </a>
+    <div>
+      <Navbar /> {/* Render the Navbar component at the top */}
+      <section className="hero-section">
+        <div>
+          <div data-aos="fade-up" className="flex-hero">
+              <h1>
+                Your Health<br/>
+                <span className="text-gradient">
+                  Our Responsibility
+                </span>
+              </h1>
+                <div class="blob-cont">
+                    <div class="blue blob"></div>
+                </div>
+                <div class="blob-cont">
+                    <div class="blue1 blob"></div>
+                </div>
+              <h4>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
+              </h4>
+              <a href="#services">
+                <button class="button">Get Started</button>
+              </a>
+          </div>
         </div>
-
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
